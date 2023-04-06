036eadac0697cbd79288026e1d09349b8af5d5b7
Artem Sydorov <sydorovay@gmail.com>
2023-04-06 21:21:21 +0200

    finished task 8-1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,32 @@
-
+//  посилання на кнопки
+const startButton = document.querySelector('[data-start]');
+const stopButton = document.querySelector('[data-stop]');
+// ідентифікатор інтервалу
+let interval = null;
+// Ф-ція випадкового кольору
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, '0')}`;
+}
+// Ф-ція призначення кольору для body
+function changeBodyColor() {
+  document.body.style.backgroundColor = getRandomHexColor();
+}
+// Ф-ція зміни колюру з інтервалом і відключенням кнопки start
+function changeColor() {
+  if (!interval) {
+    interval = setInterval(changeBodyColor, 1000);
+    startButton.disabled = true;
+  }
+}
+// ф-ція зупинки зміни колюру з активацією кнопки start
+function stopChangeBackgroundColor() {
+  clearInterval(interval);
+  interval = null;
+  startButton.disabled = false;
+}
+// обробник події 'click' для кнопки start
+startButton.addEventListener('click', changeColor);
+// обробник події 'click' для кнопки stop
+stopButton.addEventListener('click', stopChangeBackgroundColor);
