d56b84107ff9a779b4e48d6e63a46f1eec2c9e86
sydorovay <sydorovay@gmail.com>
2023-04-06 19:21:50 +0000

    Deploying to gh-pages from @ sydorovay/goit-js-hw-09@036eadac0697cbd79288026e1d09349b8af5d5b7 🚀

---
+++ 01-color-switcher.e674fbca.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]"),n=null;function a(){document.body.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,"0"))}t.addEventListener("click",(function(){n||(n=setInterval(a,1e3),t.disabled=!0)})),e.addEventListener("click",(function(){clearInterval(n),n=null,t.disabled=!1}))}();
+//# sourceMappingURL=01-color-switcher.e674fbca.js.map
---
+++ 01-color-switcher.fa0a7175.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]");let n=null;function a(){document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,"0")}`}t.addEventListener("click",(function(){n||(n=setInterval(a,1e3),t.disabled=!0)})),e.addEventListener("click",(function(){clearInterval(n),n=null,t.disabled=!1}));
+//# sourceMappingURL=01-color-switcher.fa0a7175.js.map
